83fa37f73ee385785e424994f999ceaa81aceb59
Tianyang Zhang <353613258@qq.com>
2020-01-11 21:21:18 -0500

    done

--- assignment1/collectSubmission.sh
+++
@@ -1,2 +0,0 @@
-rm -f assignment1.zip 
-zip -r assignment1.zip . -x "*.git*" "*datasets/cifar-10-batches-py*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt"
--- assignment2/README.md
+++
@@ -1,1 +0,0 @@
-Details about this assignment can be found [on the course webpage](https://compsci697l.github.io/assignments.html).
